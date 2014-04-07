2de5b39305d257662340450676e3966718b3c8c3
André Röhrig <andre.roehrig@fu-berlin.de>
2014-04-07 16:04:39 +0200

    using all files in a folder

--- xml_freq.py
+++ xml_freq.py
@@ -1,15 +1,19 @@
+import os,sys
 import xml.etree.ElementTree as ET
-tree = ET.parse('generic.xml')
-root = tree.getroot()
+dirs = os.listdir("./")
 
 freqdict = {}
 
-for i in root.iter(None):
-    elem = i.tag
-    if elem in freqdict:
-        freqdict[elem]+=1
-    else:
-        freqdict[elem]=0
+for file in dirs:
+	if file.endswith(".xml"):
+		tree = ET.parse(file)
+		root = tree.getroot()
 
+		for i in root.iter(None):
+	    		elem = i.tag
+  	  		if elem in freqdict:
+ 		       		freqdict[elem]+=1
+    			else:
+        			freqdict[elem]=1
 
 print(freqdict)
